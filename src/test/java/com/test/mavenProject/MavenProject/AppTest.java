7498599631443c7f98a3918aa2d4323f41d2c499
bharathkalapakuri@hotmail.com <bharathkalapakuri@hotmail.com>
2021-03-26 18:31:29 +0530

    updated2

--- src/test/java/com/test/mavenProject/MavenProject/AppTest.java
+++ src/test/java/com/test/mavenProject/MavenProject/AppTest.java
@@ -25,7 +25,7 @@ public class AppTest
     @Test
     public void shouldAnswerWithFalse()
     { 
-    	System.out.println("Welcome this is Passed test");
-        assertTrue(false);
+    	System.out.println("Welcome this is Failed test");
+        assertTrue(true);
     }
 }
