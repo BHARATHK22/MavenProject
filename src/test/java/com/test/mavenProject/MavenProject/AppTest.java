b6464c395be29b630b20b80ca3e6ea29b9d74216
bharathkalapakuri@hotmail.com <bharathkalapakuri@hotmail.com>
2021-04-04 23:21:20 +0530

    launchAndCloseBrowser

--- src/test/java/com/test/mavenProject/MavenProject/AppTest.java
+++ src/test/java/com/test/mavenProject/MavenProject/AppTest.java
@@ -2,6 +2,10 @@ package com.test.mavenProject.MavenProject;
 
 import static org.junit.Assert.assertTrue;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 //import static org.junit.Assert.assertTrue;
@@ -27,5 +31,16 @@ public class AppTest
     { 
     	System.out.println("Welcome this is Failed test");
         assertTrue(true);
+    }
+    @Test
+    public void launchAndCloseBrowser()
+    { 
+    	System.out.println("Welcome this is Failed test");
+    	System.setProperty("webdriver.chrome.driver", "E:\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.co.in");
+		driver.manage().window().maximize();   
+		driver.findElement(By.name("q")).sendKeys("Jenkins Pipeline",Keys.ENTER);
+		driver.quit();
     }
 }
