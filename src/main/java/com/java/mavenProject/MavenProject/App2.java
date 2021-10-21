9d8ee666c167ee2699f001232bce0e4602fefe64
bharathkalapakuri@hotmail.com <bharathkalapakuri@hotmail.com>
2021-10-21 16:20:46 +0530

    sample1

---
+++ src/main/java/com/java/mavenProject/MavenProject/App2.java
@@ -0,0 +1,10 @@
+package com.java.mavenProject.MavenProject;
+
+public class App2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Creating jar file");
+	}
+
+}
---
+++ src/test/java/com/test/mavenProject/GenericPackage/Generic2.java
@@ -0,0 +1,9 @@
+package com.test.mavenProject.GenericPackage;
+
+public class Generic2{
+	public static String inputfolder = System.getProperty("user.dir")+"\\InputFiles";
+	public String welcome = null;
+	public String tojava = null;
+	public String world = null;
+	public String IamBharath = null;
+}
