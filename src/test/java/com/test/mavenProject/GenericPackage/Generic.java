25e98c48c9ea25999fd5b076ab4f0edb0311544c
bharathkalapakuri@hotmail.com <bharathkalapakuri@hotmail.com>
2021-10-21 15:42:38 +0530

    5th commit

--- src/test/java/com/test/mavenProject/GenericPackage/Generic.java
+++ src/test/java/com/test/mavenProject/GenericPackage/Generic.java
@@ -4,4 +4,5 @@ public class Generic{
 	public static String inputfolder = System.getProperty("user.dir")+"\\InputFiles";
 	public String welcome = null;
 	public String tojava = null;
+	public String world = null;
 }
