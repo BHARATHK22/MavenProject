2ac4b55f070c85a6e20a9bac89def91e1e319321
bharathkalapakuri@hotmail.com <bharathkalapakuri@hotmail.com>
2024-04-09 21:44:24 +0530

    new pipeline

--- src/main/java/com/java/mavenProject/GenericPackage/CMD.java
+++
@@ -1,9 +0,0 @@
-package com.java.mavenProject.GenericPackage;
-
-
-	
-	public enum CMD {
-
-	Navigate,InputData,Click,Quit,IsDisplayed;
-	}
-
--- src/test/java/com/test/mavenProject/GenericPackage/Generic.java
+++
@@ -1,9 +0,0 @@
-package com.test.mavenProject.GenericPackage;
-
-public class Generic{
-	public static String inputfolder = System.getProperty("user.dir")+"\\InputFiles";
-	public String welcome = null;
-	public String tojava = null;
-	public String world = null;
-	public String IamBharath = null;
-}
